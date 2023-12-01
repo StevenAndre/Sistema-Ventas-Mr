d8aecab2004aea4e71f5bf124be22310920edcb9
Steven Cardenas V <stevencardenasvela@gmail.com>
2023-12-01 15:19:16 -0500

    avance login basico

---
+++ src/main/java/LoginFrame.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class LoginFrame extends JFrame {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+
+    public LoginFrame() {
+        setTitle("Login");
+        setSize(280, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        JLabel userLabel = new JLabel("Usuario:");
+        JLabel passLabel = new JLabel("Contraseña:");
+        usernameField = new JTextField(15);
+        passwordField = new JPasswordField(15);
+        JButton loginButton = new JButton("Iniciar Sesión");
+
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                if (username.equals("usuario") && password.equals("11111")) {
+                    dispose();
+                    openMainFrame();
+                } else {
+                    JOptionPane.showMessageDialog(LoginFrame.this, "Usuario o contraseña incorrectos", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        panel.add(userLabel);
+        panel.add(usernameField);
+        panel.add(passLabel);
+        panel.add(passwordField);
+        panel.add(loginButton);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    private void openMainFrame() {
+        MenuPrincipal mainFrame = new MenuPrincipal();
+        mainFrame.setVisible(true);
+    }
+
+
+
+}
---
+++ src/main/java/MenuPrincipal.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MenuPrincipal extends JFrame {
+        public MenuPrincipal() {
+            setTitle("Pantalla Principal");
+            setSize(400, 300);
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setLocationRelativeTo(null);
+
+
+        }
+}
+
+
